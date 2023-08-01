e8ae15f543b2d76afc1264b929aa06520de64faf
jdbm69 <104793276+jdbm69@users.noreply.github.com>
2023-08-01 11:17:40 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,12 +1,21 @@
 export const dataProjects = [
     {
+        name: 'JMDB',
+        technology: 'Next, Sass, Node.js, PostgreSQL',
+        date: 'July 2023',
+        info: 'Wtach all information of movies an persons of the industry. You can save and rate your favorites movies to your account',
+        img1: 'fitone',
+        repository: 'https://github.com/jdbm69/JMDB',
+        web: 'https://fitapp69.up.railway.app'
+    },
+    {
         name: 'Fit App',
         technology: 'React, Css, Html, Node.js, Express.js, PostgreSQL',
         date: 'March 2023',
         info: 'Version 1 of one personal project using Node and React. In the app you can register your workouts, see all your records and your process with a chart. It have a data base with more of 30 exercises for every part of the body. Comming soon version 2 with React Native',
         img1: 'fitone',
         repository: 'https://github.com/jdbm69/FitAppBack',
-        web: 'https://fitapp69.up.railway.app'
+        web: 'https://jhonnymdb.vercel.app'
     },
     {
         name: 'To-Do App',
