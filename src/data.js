78d51ff177794d30b4b733ef529b248e0a063c46
jdbm69 <104793276+jdbm69@users.noreply.github.com>
2025-03-03 14:01:06 -0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -7,14 +7,5 @@ export const dataProjects = [
         img1: 'JMDB',
         repository: 'https://github.com/jdbm69/JMDB',
         web: 'https://jhonnymdb.vercel.app/'
-    },
-    {
-        name: 'Fit App',
-        technology: 'React, Css, Html, Node.js, Express.js, PostgreSQL',
-        date: 'March 2023',
-        info: 'Version 1 of one personal project using Node and React. In the app you can register your workouts, see all your records and your process with a chart. It have a data base with more of 30 exercises for every part of the body. Comming soon version 2 with React Native',
-        img1: 'fitone',
-        repository: 'https://github.com/jdbm69/FitAppBack',
-        web: 'https://jhonnymdb.vercel.app'
     }
 ];
