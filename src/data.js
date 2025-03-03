406fe0daebe0d60fb03b5acb169bc03e354c250f
jdbm69 <104793276+jdbm69@users.noreply.github.com>
2025-03-03 13:47:44 -0300

    Update data.js

--- src/data.js
+++ src/data.js
@@ -18,48 +18,4 @@ export const dataProjects = [
         web: 'https://jhonnymdb.vercel.app'
     },
     {
-        name: 'To-Do App',
-        technology: 'React, Css, Html, Node.js, Express.js, PostgreSQL',
-        date: 'December 2022',
-        info: 'First full-stack app using Node for the backend and React for the frontend. Log in and begin register your tasks',
-        img1: 'todoone',
-        repository: 'https://github.com/jdbm69/ToDoAppBack',
-        web: 'https://todoapp69.up.railway.app'
-    },
-    {
-        name: 'First Portafolio',
-        technology: 'React, Sass, Html',
-        date: 'October 2022',
-        info: 'First personal portafolio as frontend developer',
-        img1: 'portone',
-        repository: 'https://github.com/jdbm69/Portafolio',
-        web: 'https://jhonnybracho.netlify.app'
-    },
-    {
-        name: 'Cicis Shop',
-        technology: 'React, Sass, Html, Bootstrap',
-        date: 'August 2022',
-        info: 'Version 1 of an ecommerce website for a client, comming soon version 2 with a data base with all products and the process of sign up',
-        img1: 'cicisone',
-        repository: 'https://github.com/jdbm69/CicisShop',
-        web: 'https://cicisshop.netlify.app'
-    },
-    {
-        name: 'Calculator',
-        technology: 'React, Sass, Html',
-        date: 'May 2022',
-        info: 'Second personal project of a simple calculator',
-        img1: 'calone',
-        repository: 'https://github.com/jdbm69/Calculadora',
-        web: 'https://calculadora69.netlify.app'
-    },
-    {
-        name: 'Rock Paper Scissors',
-        technology: 'React, Sass, Html',
-        date: 'May 2022',
-        info: 'First personal project of the game rock paper scissors',
-        img1: 'pptone',
-        repository: 'https://github.com/jdbm69/PiedraPapelTijera',
-        web: 'https://piedrapapeltijera69.netlify.app'
-    },
 ]
